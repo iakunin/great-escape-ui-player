e384880e1a58f25bdce73a9242248fd223db24be
iakunin <iakunin@users.noreply.github.com>
2020-11-12 21:58:28 +0300

    App refactoring

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from '../Header';
+import Footer from "../Footer";
+import Faq from "../Faq";
 import './App.scss';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* @TODO: maybe we can get rid of this `overlay`? */}
+      <div id="overlay"/>
+
+      {/* @TODO: maybe we can get rid of this `wrapper` and/or `container`? */}
+      <div id="wrapper">
+        <div className="container">
+          <Header/>
+          <section>
+            {/*  Here should be the router */}
+            <Faq/>
+          </section>
+        </div>
+      </div>
+      <Footer/>
+    </>
   );
 }
-
-export default App;
