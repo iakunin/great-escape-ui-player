5454d4c6902c142986cbe58149b1e67afa32ccd0
iakunin <iakunin@users.noreply.github.com>
2020-11-13 14:02:59 +0300

    TopRightMenu is completed

--- src/Header/TopRightMenu/TopRightMenu.tsx
+++ src/Header/TopRightMenu/TopRightMenu.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import './TopRightMenu.scss'
+import cabinetIcon from './icons/cabinet.png';
+import logoutIcon from './icons/logout.png';
+import loginIcon from './icons/login.png';
+import registrationIcon from './icons/registration.png';
+import subscriptionIcon from './icons/subscription.png';
+import Button from "./Button";
 
 export default function TopRightMenu() {
+  const isLoggedIn: boolean = false;
+
+  const buttons: JSX.Element[] =
+    isLoggedIn ?
+      [
+        <Button icon={cabinetIcon} text={"Кабинет"} href={"/cabinet"}/>,
+        <Button icon={logoutIcon} text={"Выход"} href={"#"}/>,
+      ]
+      : [
+        <Button icon={loginIcon} text={"Вход"} href={"#"}/>,
+        <Button icon={registrationIcon} text={"Регистрация"} href={"#"}/>,
+        <Button icon={subscriptionIcon} text={"Подписка"} href={"#"}/>,
+      ];
+
   return (
-    <div>
-    </div>
+    <nav className="b-header-icon-nav">
+      <ul>
+        {buttons.map(button =>
+          <li>{button}</li>
+        )}
+      </ul>
+    </nav>
   );
 }
