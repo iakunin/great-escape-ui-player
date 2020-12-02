12d799d12d889ca2b5cc3a91f664c637ae4d57d2
iakunin <iakunin@users.noreply.github.com>
2020-12-02 11:57:48 +0300

    Content component

--- src/pages/QuestList/Content/Content.tsx
+++ src/pages/QuestList/Content/Content.tsx
@@ -1,48 +1,40 @@
 import React from 'react';
 import styles from './Content.module.scss'
-import loadingIcon from "./images/loading.png";
-import notFoundIcon from "./images/notfound.png";
 import {Quest as QuestModel} from "../../../models/Quest";
-import Quest from "../Quest";
-import Sorting from "../Sorting";
-
-export enum Status {
-  Loading,
-  Found,
-  NotFound,
-}
+import {usePromiseTracker} from "react-promise-tracker";
+import {Areas} from "../../../enums";
+import loadingIcon from "./images/loading.png";
+import Sorting from "./Sorting";
+import Quest from "./Quest";
+import notFoundIcon from "./images/notfound.png";
 
 export default function Content(props: {
   quests: Array<QuestModel>;
-  status: Status;
 }) {
+  const {promiseInProgress} = usePromiseTracker({area: Areas.QuestList, delay: 100});
 
-  return (
-    <div className={styles.main}>
-
-      {props.status === Status.Found &&
-      <>
-        <Sorting/>
-
-        <div className={styles.quests}>
-          {props.quests.map((quest, idx) =>
-            <Quest key={idx} quest={quest}/>
-          )}
-        </div>
-      </>}
-
-      {props.status === Status.Loading &&
+  return promiseInProgress
+    ? (
       <div className={styles.info}>
         <img src={loadingIcon} alt="loading"/>
         <span>Мы подбираем квесты: пожалуйста, подождите</span>
-      </div>}
-
-      {props.status === Status.NotFound &&
-      <div className={styles.info}>
-        <img src={notFoundIcon} alt="not found"/>
-        <span>К сожалению, мы не смогли найти квест под Ваш запрос</span>
-      </div>}
-
-    </div>
-  );
+      </div>
+    ) : (
+      props.quests.length !== 0
+        ? (
+          <>
+            <Sorting/>
+            <div className={styles.quests}>
+              {props.quests.map((quest, idx) =>
+                <Quest key={idx} quest={quest}/>
+              )}
+            </div>
+          </>
+        ) : (
+          <div className={styles.info}>
+            <img src={notFoundIcon} alt="not found"/>
+            <span>К сожалению, мы не смогли найти квест под Ваш запрос</span>
+          </div>
+        )
+    );
 }
