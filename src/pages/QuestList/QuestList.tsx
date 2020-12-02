0273388facd05f9a6ae2a2f735b1a349e27896d4
iakunin <iakunin@users.noreply.github.com>
2020-12-02 11:58:36 +0300

    QuestList component

--- src/pages/QuestList/QuestList.tsx
+++ src/pages/QuestList/QuestList.tsx
@@ -1,16 +1,14 @@
 import React, {Component} from 'react';
-import './QuestList.scss'
 import Search from "./Search";
 import Content from "./Content";
 import {Quest} from "../../models";
-import {Status} from "./Content/Content";
+import styles from "./QuestList.module.scss";
 
 type ComponentProps = {
   /*_*/
 };
 
 type ComponentState = {
-  status: Status
   quests: Array<Quest>;
 };
 
@@ -18,30 +16,20 @@ type ComponentState = {
 export default class QuestList extends Component<ComponentProps, ComponentState> {
   constructor(props: ComponentProps) {
     super(props);
-    this.state = {
-      status: Status.Loading,
-      quests: [], // @TODO: here should be quest lists with default filters
-    };
+    this.state = {quests: []};
     this.handleQuestsChange = this.handleQuestsChange.bind(this);
-    this.handleStatusChange = this.handleStatusChange.bind(this);
-  }
-
-  private handleQuestsChange(quests: Array<Quest>): void {
-    this.setState(() => ({
-      quests: quests
-    }));
-  }
-
-  private handleStatusChange(status: Status): void {
-    this.setState(() => ({
-      status: status
-    }));
   }
 
   render = (): JSX.Element => (
     <>
-      <Search onQuestsChange={this.handleQuestsChange} onStatusChange={this.handleStatusChange}/>
-      <Content quests={this.state.quests} status={this.state.status}/>
+      <Search onQuestsChange={this.handleQuestsChange}/>
+      <div className={styles.content}>
+        <Content quests={this.state.quests}/>
+      </div>
     </>
   );
+
+  private handleQuestsChange(quests: Array<Quest>): void {
+    this.setState(() => ({quests: quests}));
+  }
 }
