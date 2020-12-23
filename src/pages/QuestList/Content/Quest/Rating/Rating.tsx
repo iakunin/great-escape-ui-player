0bc4b37e1e202d159749c76c50db84fa8b620304
Maksim Iakunin <yakuninm+github@gmail.com>
2020-12-23 21:53:07 +0300

    Getting rid of .clearfix in Rating component

--- src/pages/QuestList/Content/Quest/Rating/Rating.tsx
+++ src/pages/QuestList/Content/Quest/Rating/Rating.tsx
@@ -4,23 +4,28 @@ import styles from './Rating.module.scss';
 export default function Rating(props: {
   votesCount: number;
   likesPercent: number;
-} ): JSX.Element {
+}): JSX.Element {
   return (
     <div className={styles.main}>
-      <div className={styles.votesCount}>{props.votesCount}</div>
-      <div className={styles.likesPercent}>
-        Понравилось {props.likesPercent}%
-      </div>
-      <div className="clearfix"/>
 
-      <div className={styles.minus}/>
-      <div className={styles.scale}>
-        <div
-          className={styles.progress}
-          style={{width: `${props.likesPercent}%`}}
-        />
+      <div className={styles.top}>
+        <div className={styles.votesCount}>{props.votesCount}</div>
+        <div className={styles.likesPercent}>
+          Понравилось {props.likesPercent}%
+        </div>
       </div>
-      <div className={styles.plus}/>
+
+      <div className={styles.bottom}>
+        <div className={styles.minus}/>
+        <div className={styles.scale}>
+          <div
+            className={styles.progress}
+            style={{width: `${props.likesPercent}%`}}
+          />
+        </div>
+        <div className={styles.plus}/>
+      </div>
+
     </div>
   );
 }
