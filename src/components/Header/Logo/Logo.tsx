b83dbaa5c50d8e8e1bb51f29cf99b4f04352d1a6
Maksim Iakunin <yakuninm+github@gmail.com>
2020-12-24 10:45:13 +0300

    Logo: Added Link to Home page

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -2,11 +2,29 @@ import React from 'react';
 import SocialButtons from 'components/SocialButtons';
 import styles from './Logo.module.scss';
 import {sample} from 'lodash';
+import {withRouter} from 'react-router-dom';
+import {RouteComponentProps} from 'react-router';
+import {Routes} from 'enums/Routes';
 
-export default function Logo(): JSX.Element {
+const texts: JSX.Element[] = [
+  <>
+    <span>Мы знаем</span>о квестах все!
+  </>,
+  <>
+    <span>Лучшие</span>квесты Москвы!
+  </>,
+  <>
+    <span>Вступай</span>скорее в клуб!
+  </>,
+];
+
+function Logo(props: RouteComponentProps): JSX.Element {
   return (
     <div className={styles.main}>
-      <div className={styles.image}/>
+      <div
+        className={styles.image}
+        onClick={(): void => props.history.push(Routes.Home)}
+      />
       <div className={styles.text}>
 
         <ul>
@@ -24,14 +42,4 @@ export default function Logo(): JSX.Element {
   );
 }
 
-const texts: JSX.Element[] = [
-    <>
-      <span>Мы знаем</span>о квестах все!
-    </>,
-    <>
-      <span>Лучшие</span>квесты Москвы!
-    </>,
-    <>
-      <span>Вступай</span>скорее в клуб!
-    </>,
-];
+export default withRouter(Logo);
