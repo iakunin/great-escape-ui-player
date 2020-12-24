c54823e60e06e9cb8c2b8a5d3d559c97e1aba0ec
Maksim Iakunin <yakuninm+github@gmail.com>
2020-12-24 11:14:28 +0300

    Footer: getting rid of .clearfix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,15 +6,15 @@ import appConfig from 'config/appConfig';
 export default function Footer(): JSX.Element {
   return (
     <footer className={styles.main}>
-      <div className={styles.inner}>
-        <div className={styles.copyright}>© 2020, Great&nbsp;Escape</div>
-        <div className={styles.support}>Ответим на любые вопросы —
-          <a href={`mailto:${appConfig.email.info}`}>{appConfig.email.info}</a>
+        <div className={styles.copyright}>© 2020, Great Escape</div>
+        <div className={styles.support}>
+          Ответим на любые вопросы — <a href={`mailto:${appConfig.email.info}`}>
+            {appConfig.email.info}
+          </a>
         </div>
         <div className={styles.socialButtons}>
           <SocialButtons/>
         </div>
-      </div>
     </footer>
   );
 }
