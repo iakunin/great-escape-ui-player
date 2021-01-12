e8a0db7f1861f86ccb923eef8a9d98bd6df57bdc
Maksim Iakunin <yakuninm+github@gmail.com>
2021-01-12 11:09:52 +0300

    Changing date in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import appConfig from 'config/appConfig';
 export default function Footer(): JSX.Element {
   return (
     <footer className={styles.main}>
-        <div className={styles.copyright}>© 2020, Great Escape</div>
+        <div className={styles.copyright}>© 2020-2021, Great Escape</div>
         <div className={styles.support}>
           Ответим на любые вопросы — <a href={`mailto:${appConfig.email.info}`}>
             {appConfig.email.info}
