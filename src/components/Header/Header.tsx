90cbdea111e4ef47845f2320be29c056a05e5095
Maksim Iakunin <yakuninm+github@gmail.com>
2021-02-10 13:39:34 +0300

    Header: burger was added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import styles from './Header.module.scss';
 import Navigation from './Navigation';
 import Logo from './Logo';
-import ButtonsBlock from './ButtonsBlock';
 
 export default function Header(): JSX.Element {
   return (
     <div className={styles.main}>
       <Logo/>
       <Navigation/>
-      <ButtonsBlock/>
+      {/*<ButtonsBlock/>*/}
     </div>
   );
 }
--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -5,19 +5,24 @@ import {Routes} from 'enums/Routes';
 
 export default function Navigation(): JSX.Element {
   return (
-    <nav className={styles.main}>
-      <ul className={styles.list}>
-        <NavLink exact to={Routes.Home} activeClassName={styles.active} component={CustomNavLink}>
-          Квесты
-        </NavLink>
-        <NavLink exact to={Routes.Faq} activeClassName={styles.active} component={CustomNavLink}>
-          FAQ
-        </NavLink>
-        <NavLink exact to={Routes.Contacts} activeClassName={styles.active} component={CustomNavLink}>
-          Контакты
-        </NavLink>
-      </ul>
-    </nav>
+    <>
+      <nav className={styles.main}>
+        <ul className={styles.list}>
+          <NavLink exact to={Routes.Home} activeClassName={styles.active} component={CustomNavLink}>
+            Квесты
+          </NavLink>
+          <NavLink exact to={Routes.Faq} activeClassName={styles.active} component={CustomNavLink}>
+            FAQ
+          </NavLink>
+          <NavLink exact to={Routes.Contacts} activeClassName={styles.active}
+                   component={CustomNavLink}>
+            Контакты
+          </NavLink>
+        </ul>
+      </nav>
+      {/* @TODO: при клике на бургер показывать боковое меню (как у https://quest.haus/) */}
+      <button className={styles.burger}/>
+    </>
   );
 }
 
