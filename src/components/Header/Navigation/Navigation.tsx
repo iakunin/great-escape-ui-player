7b55c557b0587056b9feef46de174ed4aaffb8f4
Maksim Iakunin <yakuninm+github@gmail.com>
2021-02-13 17:03:17 +0300

    Header: added pop-up with nav-menu

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -1,29 +1,46 @@
-import React, {Component} from 'react';
+import React, {Component, MouseEventHandler, useState} from 'react';
 import styles from './Navigation.module.scss';
 import {Link, NavLink} from 'react-router-dom';
 import {Routes} from 'enums/Routes';
+import ReactPopup from 'reactjs-popup';
 
 export default function Navigation(): JSX.Element {
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  const links = (): Array<JSX.Element> => (
+    [
+      {to: Routes.Home, title: 'Квесты'},
+      {to: Routes.Faq, title: 'FAQ'},
+      {to: Routes.Contacts, title: 'Контакты'},
+    ].map((link, idx) => (
+      <NavLink exact key={idx} to={link.to} activeClassName={styles.active}
+               component={CustomNavLink} onClick={() => setPopupOpen(false)}
+      >
+        {link.title}
+      </NavLink>
+    ))
+  );
+
   return (
     <>
       <nav className={styles.main}>
         <ul className={styles.list}>
-          <NavLink exact to={Routes.Home} activeClassName={styles.active} component={CustomNavLink}>
-            Квесты
-          </NavLink>
-          <NavLink exact to={Routes.Faq} activeClassName={styles.active} component={CustomNavLink}>
-            FAQ
-          </NavLink>
-          <NavLink exact to={Routes.Contacts} activeClassName={styles.active}
-                   component={CustomNavLink}>
-            Контакты
-          </NavLink>
+          {links()}
         </ul>
       </nav>
 
-      {/* @TODO: при клике на бургер показывать боковое меню (как у https://quest.haus/) */}
+      <button className={styles.burger} onClick={() => setPopupOpen(true)}/>
 
-      <button className={styles.burger}/>
+      <ReactPopup open={isPopupOpen} closeOnDocumentClick onClose={() => setPopupOpen(false)}
+                  overlayStyle={{background: 'rgba(0,0,0,0.85)'}}
+                  contentStyle={{margin: '25% auto', height: 'fit-content'}}
+      >
+        <nav className={styles.mainPopup}>
+          <ul className={styles.listPopup}>
+            {links()}
+          </ul>
+        </nav>
+      </ReactPopup>
     </>
   );
 }
@@ -31,10 +48,11 @@ export default function Navigation(): JSX.Element {
 class CustomNavLink extends Component<{
   href: string;
   className?: string;
+  onClick?: MouseEventHandler;
 }> {
   render = (): JSX.Element => (
     <li className={this.props.className ?? ''}>
-      <Link to={this.props.href}>{this.props.children}</Link>
+      <Link to={this.props.href} onClick={this.props.onClick}>{this.props.children}</Link>
     </li>
   );
 }
