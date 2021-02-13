4e6ca727f7c1cab3acf7c42b74c8ba230a7cfe0d
Maksim Iakunin <yakuninm+github@gmail.com>
2021-02-13 18:40:19 +0300

    Quest in QuestList page: changing layout

--- src/pages/QuestList/Content/Content.tsx
+++ src/pages/QuestList/Content/Content.tsx
@@ -4,7 +4,6 @@ import {QuestList} from 'models/Quest';
 import {trackPromise, usePromiseTracker} from 'react-promise-tracker';
 import {Areas} from 'enums/Areas';
 import loadingIcon from './images/loading.png';
-import Sorting from './Sorting';
 import notFoundIcon from './images/notfound.png';
 import {getQuestList} from 'api/getQuestList';
 import Quest from './Quest';
@@ -31,37 +30,28 @@ function Content(props: QuestListProps): JSX.Element {
 
   if (promiseInProgress || questList === undefined) {
     return (
-      <>
-        <Sorting/>
-        <div className={styles.info}>
-          <img src={loadingIcon} alt="loading"/>
-          <span>Мы подбираем квесты: пожалуйста, подождите</span>
-        </div>
-      </>
+      <div className={styles.info}>
+        <img src={loadingIcon} alt="loading"/>
+        <span>Мы подбираем квесты: пожалуйста, подождите</span>
+      </div>
     );
   }
 
   if (questList.length === 0) {
     return (
-      <>
-        <Sorting/>
-        <div className={styles.info}>
-          <img src={notFoundIcon} alt="not found"/>
-          <span>К сожалению, мы не смогли найти квест под Ваш запрос</span>
-        </div>
-      </>
+      <div className={styles.info}>
+        <img src={notFoundIcon} alt="not found"/>
+        <span>К сожалению, мы не смогли найти квест под Ваш запрос</span>
+      </div>
     );
   }
 
   return (
-    <>
-      <Sorting/>
-      <div className={styles.quests}>
-        {questList.map((quest, idx) =>
-          <Quest key={idx} quest={quest}/>
-        )}
-      </div>
-    </>
+    <div className={styles.quests}>
+      {questList.map((quest, idx) =>
+        <Quest key={idx} quest={quest}/>
+      )}
+    </div>
   );
 }
 
--- src/pages/QuestList/QuestList.tsx
+++ src/pages/QuestList/QuestList.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Search from './Search';
 import Content from './Content';
 import styles from './QuestList.module.scss';
+import Sorting from './Sorting';
 
 export default function QuestList(): JSX.Element {
   return (
     <>
       <Search/>
+      <Sorting/>
       <div className={styles.content}>
         <Content/>
       </div>
